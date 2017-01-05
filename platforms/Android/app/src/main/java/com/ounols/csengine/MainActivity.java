248c3f3035b369eb416c715d4d315c879518f88f
ounols <ounols@naver.com>
2017-01-06 00:42:10 +0900

    First Initialized

---
+++ platforms/Android/app/src/main/java/com/ounols/csengine/MainActivity.java
@@ -0,0 +1,12 @@
+package com.ounols.csengine;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
