5b7db67ed190bbab350dff42e869a7647642c6ec
ounols <ounols@naver.com>
2022-12-06 03:51:21 +0900

    Improved MoreString header (20221206)

--- src/Util/MoreString.h
+++ src/Util/MoreString.h
@@ -10,11 +10,9 @@
 namespace CSE {
 
     static std::string ReplaceAll(std::string& str, const std::string& from, const std::string& to) {
-        size_t start_pos = 0; //string ??????? ???
-        while ((start_pos = str.find(from, start_pos)) != std::string::npos)  //from?? ??? ?? ???? ??????
-        {
+        size_t start_pos = 0;
+        while ((start_pos = str.find(from, start_pos)) != std::string::npos) {
             str.replace(start_pos, from.length(), to);
-            start_pos += to.length(); // ?????? ????? from.length() > to.length()?? ??? ?????
         }
         return str;
     }
@@ -22,12 +20,11 @@ namespace CSE {
     static std::string
     ReplaceFunction(std::string& str, const std::string& from, const std::string& from2, const std::string& to,
                     const std::string& to2) {
-        size_t start_pos = 0; //string ??????? ???
+        size_t start_pos = 0;
         size_t start_pos2 = 0;
-        while ((start_pos = str.find(from, start_pos)) != std::string::npos)  //from?? ??? ?? ???? ??????
-        {
+        while ((start_pos = str.find(from, start_pos)) != std::string::npos) {
             str.replace(start_pos, from.length(), to);
-            start_pos += to.length(); // ?????? ????? from.length() > to.length()?? ??? ?????
+            start_pos += to.length();
 
             if ((start_pos2 = str.find(from2, start_pos)) != std::string::npos) {
                 if (str.find(from, start_pos) < start_pos2) continue;
@@ -45,18 +42,31 @@ namespace CSE {
         return r.erase(0, r.find_first_not_of(" \n\t\r"));
     }
 
-    static std::vector<std::string> split(const std::string& s, char seperator) {
+    static std::vector<std::string> split(const std::string& s, char separator) {
         std::vector<std::string> output;
 
-        std::string::size_type prev_pos = 0, pos = 0;
+        size_t prev_pos = 0;
+        size_t pos = 0;
 
-        while ((pos = s.find(seperator, pos)) != std::string::npos) {
-            std::string substring(s.substr(prev_pos, pos - prev_pos));
-            output.push_back(substring);
+        // 문자열을 일부분만 저장하고 나머지는 버퍼에 저장합니다.
+        // 이렇게 하면 입력 문자열의 길이에 상관없이
+        // 항상 정해진 크기의 버퍼를 사용하기 때문에 성능이 향상됩니다.
+        constexpr size_t BUFFER_SIZE = 64;
+        char buffer[BUFFER_SIZE];
+
+        while ((pos = s.find(separator, pos)) != std::string::npos) {
+            // 입력 문자열의 일부분을 출력 벡터에 저장합니다.
+            output.push_back(s.substr(prev_pos, pos - prev_pos));
             prev_pos = ++pos;
         }
 
-        output.push_back(s.substr(prev_pos, pos - prev_pos)); // Last word
+        // 나머지 문자열을 버퍼에 저장합니다.
+        const size_t length = s.length() - prev_pos;
+        s.copy(buffer, length, prev_pos);
+        buffer[length] = '\0';
+
+        // 버퍼에 저장된 나머지 문자열을 출력 벡터에 추가합니다.
+        output.emplace_back(buffer);
 
         return output;
     }
@@ -91,14 +101,25 @@ namespace CSE {
         return appandAll(sstream, paramN...);
     }
 
-    static std::string ConvertSpaceStr(std::string str, bool SpaceNotChange = false) {
-        auto n = str.find(' ');
-        if (n == std::string::npos || SpaceNotChange) {
-            n = str.find("$nbsp:");
-            if (n != std::string::npos)
-                return ReplaceAll(str, "$nbsp:", " ");
-            return str;
+    static std::string ConvertSpaceStr(const std::string& str, bool SpaceNotChange = false) {
+        std::string converted_str = str;
+
+        if (SpaceNotChange) {
+            // $nbsp: 문자열을 공백 문자로 대체
+            size_t pos = 0;
+            while ((pos = converted_str.find("$nbsp:", pos)) != std::string::npos) {
+                converted_str.replace(pos, 6, " ");
+                pos += 1;
+            }
+        } else {
+            // 공백 문자를 $nbsp: 문자열로 대체
+            size_t pos = 0;
+            while ((pos = converted_str.find(' ', pos)) != std::string::npos) {
+                converted_str.replace(pos, 1, "$nbsp:");
+                pos += 6;
+            }
         }
-        return ReplaceAll(str, " ", "$nbsp:");
+
+        return converted_str;
     }
 }
